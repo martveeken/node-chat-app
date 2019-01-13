3ce93ddb38dd7f2b16cf4331eb45f6772f815492
martveeken <mart@inkstrategy.com>
2019-01-13 10:21:03 +0100

    patch

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.port || 3000;
-
 const app = express();
 
 app.use(express.static(publicPath));
