ac03d55bf53583e3460aa28e1f11ab5e35257aa1
martveeken <mart@inkstrategy.com>
2019-01-15 14:51:16 +0100

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createEmail', {
-    to: 'mart.veeken@gmail.com',
-    text: 'Hi this is Mart'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'mart.veeken@gmail.com',
+  //   text: 'Hi this is Mart'
+  // });
 });
 
 socket.on('disconnect', function () {
@@ -15,8 +15,4 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log(message);
-
-  socket.emit('createMessage', function () {
-    console.log('Great dude!');
-  });
 })
